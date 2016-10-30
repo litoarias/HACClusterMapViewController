30ab96f1ff8179c39768cfabb53d115312a8305a
litoarias <lito.arias.cervero@gmail.com>
2016-10-30 14:47:19 +0100

    Shared schemes

---
+++ HACClusterMapViewController/HACClusterMapViewController.h
@@ -0,0 +1,19 @@
+//
+//  HACClusterMapViewController.h
+//  HACClusterMapViewController
+//
+//  Created by Hipolito Arias on 30/10/16.
+//  Copyright © 2016 MasterApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HACClusterMapViewController.
+FOUNDATION_EXPORT double HACClusterMapViewControllerVersionNumber;
+
+//! Project version string for HACClusterMapViewController.
+FOUNDATION_EXPORT const unsigned char HACClusterMapViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HACClusterMapViewController/PublicHeader.h>
+
+
